d8cfdd7bd88946df7a2c75398a1353d2fa7ec609
rumenmitrev <57098783+rumenmitrev@users.noreply.github.com>
2020-02-12 21:00:51 +0200

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -4,3 +4,5 @@ sudo service webodm-nginx restart
 sudo service webodm-gunicorn restart
 sudo service webodm-celery restart
 sudo service webodm-celerybeat restart
+sudo service postgresql restart
+sudo service redis-server restart
