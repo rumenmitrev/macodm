f81f5ddbd3c0286f2180c01748230e657100beb2
rumen <rumen.mitrev@abv.bg>
2020-02-12 22:46:07 +0200

    commit

--- restart.sh
+++ restart.sh
@@ -4,3 +4,9 @@ sudo service webodm-nginx restart
 sudo service webodm-gunicorn restart
 sudo service webodm-celery restart
 sudo service webodm-celerybeat restart
+sudo service postgresql restart
+sudo service redis-server restart
+sudo service clusterodm restart
+sudo service micmac restart
+sudo service nodeodm restart
+
