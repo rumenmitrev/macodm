a7ffa969899e937ee0c817156389137fe726dd79
RumenMitrev <57098783+rumenmitrev@users.noreply.github.com>
2020-05-20 16:32:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -268,6 +268,9 @@ WantedBy=multi-user.target
   cd ~
   curl -fsSL https://get.docker.com -o get-docker.sh
   sh get-docker.sh
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
   #sudo docker run -d -p 3002:3000 dronemapper/node-micmac
   #sudo sed -i '12a docker run -d -p 3002:3000 dronemapper/node-micmac' /etc/rc.local
  
