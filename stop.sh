823749d13d8c0090dd0a52cf290f1b91f7b15630
RumenMitrev <57098783+rumenmitrev@users.noreply.github.com>
2020-11-04 12:30:43 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-sudo service webodm-nginx stop
-sudo service webodm-gunicorn stop
-sudo service webodm-celery stop
-sudo service webodm-celerybeat stop
-sudo service postgresql stop
-sudo service redis-server stop
 sudo service clusterodm stop
 sudo service micmac stop
 sudo service nodeodm stop
+sudo service webodm stop
